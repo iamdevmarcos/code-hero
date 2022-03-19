573c153fc1768611d9b5ec37fbe836c7f27a6f9a
Marcos Andre <marcosdev.me@gmail.com>
2022-03-19 11:15:48 -0300

    style: add Global Styles

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  :root {
+    --background: #E5E5E5;
+  }
+
+  * {
+    margin:0;
+    padding:0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Poppins', sans-serif
+  }
+`;
