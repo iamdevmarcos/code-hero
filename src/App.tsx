d4017936a00a7a95a5869bf2f5609e8425e321ac
Marcos Andre <marcosdev.me@gmail.com>
2022-03-20 11:52:33 -0300

    feat: add pagination

--- src/App.tsx
+++ src/App.tsx
@@ -15,32 +15,19 @@ const App: React.FC = () => {
   const [loading, setLoading] = useState(false);
   const [list, setList] = useState<CharacterType[]>([]);
 
+  const [totalItems, setTotalItems] = useState(0);
+  const [pageCount, setPageCount] = useState(0);
+
   const fetchData = async () => {
     setLoading(true);
 
     const res = await Api.getCharacters();
     if (res.data.code === 200) {
       setList(res.data.data.results);
+      setTotalItems(res.data.data.total);
       setLoading(false);
     }
   };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  // const getCharacter = async () => {
-  //   setLoading(true);
-  //   const res = await Api.getSpecificCharacter();
-
-  //   if (res.data.code === 200) {
-  //     setList([]);
-  //     setList(res.data.data.results);
-  //     setLoading(false);
-  //   } else {
-  //     alert("Personagem não encontrado!");
-  //   }
-  // };
 
   const filterCharacter = (characterName: string) => {
     let newList = [...list];
@@ -54,6 +41,24 @@ const App: React.FC = () => {
       alert("Personagem não encontrado!");
     }
   };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (list.length > 0) {
+      setPageCount(Math.ceil(totalItems / list.length));
+    } else {
+      setPageCount(0);
+    }
+  }, [totalItems, list.length]);
+
+  // creating pagination array and setting limit to 20 pages
+  const pagination = [];
+  for (let i = 1; i <= 20; i++) {
+    pagination.push(i);
+  }
 
   return (
     <Container>
@@ -71,6 +76,13 @@ const App: React.FC = () => {
       <Area>
         <Content>
           <SearchArea filterCharacter={filterCharacter} fetchData={fetchData} />
+
+          <h2>Total de Paginas: {pageCount}</h2>
+          <ul>
+            {pagination.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
 
           <div className="characterArea">
             {!loading && list.length > 0 && <CharacterTable data={list} />}
