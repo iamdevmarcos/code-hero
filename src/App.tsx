d7e42d324f18108f3683a24346aecc7ca1800db3
Marcos Andre <marcosdev.me@gmail.com>
2022-03-20 12:23:40 -0300

    fix issues

--- src/App.tsx
+++ src/App.tsx
@@ -77,7 +77,9 @@ const App: React.FC = () => {
       </Area>
       <Pagination>
         {pagination.map((item, index) => (
-          <div key={index}>{item}</div>
+          <div key={index}>
+            {item} - {pageCount}
+          </div>
         ))}
       </Pagination>
     </Container>
